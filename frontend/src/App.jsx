a0581f9f2ba4d85a9b000c668f4dfa825efb2b8a
Geeorgge <jorge_hernandezvqz@hotmail.com>
2025-08-28 10:34:07 -0600

    feat: wire routes for /, /news, /news/:id and /stats under MainLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,27 @@
 import "./App.css";
-import { useState } from "react";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
-import MainLayout from "./layout/MainLayout";
 import About from "./pages/About";
+import MainLayout from "./layout/MainLayout";
+import NewsList from "./components/NewsList";
+import NewsDetail from "./components/NewsDetail";
+import StatsDashboard from "./components/StatsDashboard";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    
-    <MainLayout>
-      <Home />
-      {/* <About /> */}
-    </MainLayout>
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<NewsList />} />
+          <Route path="/news/:id" element={<NewsDetail />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/stats" element={<StatsDashboard />} />
+        </Routes>
+      </MainLayout>
+    </Router>
   );
 }
 
